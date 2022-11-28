9564187341977b4cbd881616c495e6ec7d46db9d
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-28 17:12:05 +0100

    created table with parameters

--- index.php
+++ index.php
@@ -61,11 +61,44 @@ foreach ($hotels as $hotel) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>PHP Hotel</title>
 </head>
 
 <body>
-
+    <div class="container">
+        <table class="table">
+            <thead class="bg-info">
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance_to_center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th><?php echo $hotel['name'] ?></th>
+                    <td><?php echo $hotel['description'] ?></td>
+                    <td><?php echo $hotel['parking'] ?></td>
+                    <td><?php echo $hotel['vote'] ?></td>
+                    <td><?php echo $hotel['distance_to_center'] ?></td>
+                </tr>
+                <tr>
+                    <th><?php echo $hotel['name'] ?></th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
